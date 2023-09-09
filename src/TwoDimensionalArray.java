0c2a091d3ee44867dbe5e831b22f7f9e8e3f975b
WaldyWhite <w.weis@hotmail.com>
2023-09-09 17:19:40 +0200

    Update commit

--- src/TwoDimensionalArray.java
+++ src/TwoDimensionalArray.java
@@ -1,18 +1,25 @@
-import java.util.Arrays;
-
 public class TwoDimensionalArray {
     public static void main(String[] args) {
-        String[][] arrays = {
-                {"a1", "a2", "a3", "a4", "a5", "a6"},
-                {"b1", "b2", "b3", "b4", "b5", "b6"},
-                {"c1", "c2",  "c3", "c4", "c5", "c6"}
-        };
+        String[][] arrays = new String [3][6];
+        int i, j, k;
+        for ( i = 0; i < arrays.length; i++) {
+            for (j = 0; j < arrays[i].length; j++) {
+                if (i == 0) {
+                    arrays[i][j] = "a" + (j + 1);
+                } else if (i == 1) {
+                    arrays[i][j] = "b" + (j + 1);
+                } else {
+                    arrays[i][j] = "c" + (j + 1);
+                }
+            }
+        }
+
         for (String[] el : arrays) {
-            for (int i = 0; i < el.length ; i++) {
-                if (i == el.length-1) {
+            for (k = 0; k < el.length; k++) {
+                if (k == 5) {
                     System.out.print("\n");
                 } else {
-                    System.out.print(el[i]+ " ");
+                    System.out.print(el[k] + " ");
                 }
             }
         }
